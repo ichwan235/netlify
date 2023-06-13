d60f45ddaab31c53547882e706ad007b1224c6b0
Ichwan Wahyu Utama <ichwan.20058@mhs.unesa.ac.id>
2023-06-13 18:48:26 +0700

    Pesan commit Anda di sini

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ichwan Wahyu Utama" // TODO: replace this
+const studentId = "FE4677654" // TODO: replace this
+
+export { url, studentName, studentId }
